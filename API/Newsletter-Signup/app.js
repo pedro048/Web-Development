9b237b95db57220f7719ed793a74e42e4aeb1cd7
pedro048 <pedro,andrade046@gmail.com>
2022-06-24 17:16:54 -0300

    a new commit

---
+++ API/Newsletter-Signup/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
