f4de0281b2bcd2e9ec3669dc58b90f2421ffc7ae
pedro048 <pedro,andrade046@gmail.com>
2022-06-25 13:40:45 -0300

    a new commit

--- API/Newsletter-Signup/app.js
+++ API/Newsletter-Signup/app.js
@@ -33,6 +33,13 @@ app.post("/", function(req, res){
     }
 
     const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+           /* res.send("Successfully subscribed!");  */
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            /* res.send("There was an error with signing up, please try again!"); */
+            res.sendFile(__dirname + "/failure.html");
+        }
         response.on("data", function(data){
             console.log(JSON.parse(data));
         });
@@ -42,7 +49,11 @@ app.post("/", function(req, res){
     request.end();
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
 
