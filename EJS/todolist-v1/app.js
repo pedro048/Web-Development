60d54a35e1f920b6cc72601b7271440362d9c949
pedro048 <pedro,andrade046@gmail.com>
2022-08-08 17:10:33 -0300

    a new commit

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -9,25 +9,51 @@ app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
     var day = "";
-
+    /*
     if (currentDay == 6 || currentDay == 0){ 
         //res.send("<h1 style='color: green;'>Yay it's the weekend!</h1>");
-        /*
-        res.write("<h1 style='color: green;'>Yay it's the weekend!</h1>");
-        res.write("<p>It is the weekend!</p>");
-        res.send();
-        */
+        
+        //res.write("<h1 style='color: green;'>Yay it's the weekend!</h1>");
+        //res.write("<p>It is the weekend!</p>");
+        //res.send();
+        
         day = "Weekend";
     }else{
         //res.send("<h1 style='color: red;'>Boo! I have to work!</h1>");
-        /*
-        res.write("<h1 style='color: red;'>Boo! I have to work!</h1>");
-        res.write("<p>It is not the weekend</p>");
-        res.send();
-        */
+        
+        //res.write("<h1 style='color: red;'>Boo! I have to work!</h1>");
+        //res.write("<p>It is not the weekend</p>");
+        //res.send();
+        
         //res.sendFile(__dirname + "/index.html"); 
         day = "Weekday";
     }
+    */
+    switch(currentDay){
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;  
+        default:
+        console.log("Error: current day is equal to: " + currentDay);              
+       }
     res.render("list", {kindOfDay: day});
 });
 
