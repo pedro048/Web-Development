375a296bd4f1405249c0fb811e26d3c8cf82044f
pedro048 <pedro,andrade046@gmail.com>
2022-08-08 14:04:50 -0300

    a new commit

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -3,16 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
     if (currentDay == 6 || currentDay == 0){ 
         //res.send("<h1 style='color: green;'>Yay it's the weekend!</h1>");
+        /*
         res.write("<h1 style='color: green;'>Yay it's the weekend!</h1>");
         res.write("<p>It is the weekend!</p>");
         res.send();
+        */
+        day = "Weekend";
     }else{
         //res.send("<h1 style='color: red;'>Boo! I have to work!</h1>");
         /*
@@ -20,8 +25,10 @@ app.get("/", function(req, res){
         res.write("<p>It is not the weekend</p>");
         res.send();
         */
-        res.sendFile(__dirname + "/index.html");        
+        //res.sendFile(__dirname + "/index.html"); 
+        day = "Weekday";
     }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
