8b9258407fab9b41ab59e89f14ae8f9e8aaf1ba7
pedro048 <pedro,andrade046@gmail.com>
2022-12-30 11:07:02 -0300

    a new commit

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 app.get("/", function(req, res){
     let today = new Date();
     let options = {
