de52a07f5a9248accd887792454636e39e49ff28
pedro048 <pedro,andrade046@gmail.com>
2022-12-30 16:34:44 -0300

    a new commit

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
@@ -11,13 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
     res.render("list", {listTitle: day, newListItems: items});
     /*
     var currentDay = today.getDay();
